8e6dde3b69c348755d764a1641b9887056d8315f
esttres <esttres@hotmail.com>
2015-08-20 15:02:32 -0400

    Get it working

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+end
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
+
+  Capybara.default_driver = :poltergeist
+
 
   # Add more helper methods to be used by all tests here...
 end
