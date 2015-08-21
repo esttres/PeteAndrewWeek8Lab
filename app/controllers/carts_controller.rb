f6bd3a06fc4af30df3c5bac9d0c9098a48087cbc
andrewariley87 <andrewariley87@gmail.com>
2015-08-21 13:26:30 -0400

    methods
    added more to the set_cart method in the Carts controller, only routes available to Carts are show, edit, and update

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,32 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update]
+
+
   def show
   end
 
   def edit
-  
+  end
+
+  def update
+    respond_to do |format|
+      if @cart.update(post_params)
+        format.html { redirect_to @cart, notice: 'You updated your cart!' }
+        format.json { render :show, status: :ok, location: @cart }
+      else
+        format.html { render :edit }
+        format.json { render json: @cart.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
   def set_cart
-
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :carts
+  resources :carts, only: :show, :edit, :update
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Payola::Engine => '/payola', as: :payola
   devise_for :users
