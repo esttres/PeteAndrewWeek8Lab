84d4423b396579b8b2ad0704f4479ebae140c35f
andrewariley87 <andrewariley87@gmail.com>
2015-08-23 21:01:39 -0400

    permalink
    Added permalink to products.  Will have to db:migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824003852) do
+ActiveRecord::Schema.define(version: 20150824005931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 20150824003852) do
     t.integer  "seller_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "permalink"
   end
 
   create_table "refile_attachments", force: :cascade do |t|
