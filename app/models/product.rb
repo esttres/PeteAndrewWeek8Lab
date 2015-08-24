bccdd4eef6e3e4cd4d10b3d8130baa2e9dd193e3
andrewariley87 <andrewariley87@gmail.com>
2015-08-23 20:01:04 -0400

    cart
    shopping cart works, has total at the bottom of the page.  No way to change values, or only show unique items (each of the items is a line item that was created at different time).  Do not know how to incorporate payola in order to pay.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
-  has_many :sellers, dependent: :destroy
+  has_many :line_items
+  before_destroy :not_located_in_cart
+
+  def not_located_in_cart
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "line items present"
+      return false
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  resources :line_items
+  resources :carts
   resources :carts, only: [:show, :edit, :update]
   resources :products
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
